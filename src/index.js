7039cc9342a847cdb2f0263943f886074319194c
ThachClone29 <phongcanh.net@gmail.com>
2026-05-09 21:10:14 +0700

    Fix ReactDOM render 2nd time for dashboard deployment

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// import App from './App';
-// import { Provider } from 'react-redux';
-// import { store, persistor } from "./redux/store"
-// import { PersistGate } from 'redux-persist/integration/react';
+import App from './App';
+import { Provider } from 'react-redux';
+import { store, persistor } from "./redux/store"
+import { PersistGate } from 'redux-persist/integration/react';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <Provider store={store}>
-//     <PersistGate loading={null} persistor={persistor}>
-//       <App />
-//     </PersistGate>
-//   </Provider>
-// );
-
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistor } from "./redux/store";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// import { Provider } from "react-redux";
+// import { store } from "./redux/store";
+// import { PersistGate } from "redux-persist/integration/react";
+// import { persistor } from "./redux/store";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <App />
+//       </PersistGate>
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
